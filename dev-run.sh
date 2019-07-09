d8ae13f47f9f9074d1ded5c0851924a6765ccc46
Romain Pelisse <belaran@gmail.com>
2019-07-09 14:56:39 +0200

    Fix TASKSD_PIDFILE vars

--- dev-run.sh
+++ dev-run.sh
@@ -3,6 +3,6 @@ export JAVA_HOME=${JAVA_HOME:-'/usr/lib/jvm/java-11'}
 export PATH=${JAVA_HOME}/bin:${PATH}
 
 
-export TASKS_PIDFILE=$(pwd)/taskd.pid
+export TASKSD_PIDFILE=$(pwd)/taskd.pid
 export TASKS_CLIENT_SECRET="$(pwd)/client_secret.json"
 mvn clean compile quarkus:dev -Dquarkus.http.port=8083
