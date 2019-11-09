d7f283a7c47e596cbad4392f995b64dac3ddf94f
Romain Pelisse <belaran@gmail.com>
2019-11-09 18:17:12 +0100

    Ensure JAVA_HOME is overrideable in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export JAVA_HOME=/usr/lib/jvm/java-9-openjdk/
+export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-9-openjdk/}
 export PATH=${JAVA_HOME}/bin:${PATH}
-
 
 export TASKSD_PIDFILE=$(pwd)/taskd.pid
 export TASKS_CLIENT_SECRET="$(pwd)/client_secret.json"
