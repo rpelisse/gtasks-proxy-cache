3deeae59f4dca0409046548e1deba6d1b3f3a3d7
Romain Pelisse <belaran@gmail.com>
2019-12-09 15:29:10 +0100

    Bump default jdk for build to /usr/lib/jvm/java-11-openjdk

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-9-openjdk/}
+export JAVA_HOME=${JAVA_HOME:-'/usr/lib/jvm/java-11-openjdk'}
 export PATH=${JAVA_HOME}/bin:${PATH}
 
 export TASKSD_PIDFILE=$(pwd)/taskd.pid
