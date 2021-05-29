82b1cd51f10fe5d28840087a9b2fbbd97480ef93
Romain Pelisse <belaran@gmail.com>
2021-05-29 13:08:29 +0200

    Switch packaging to uberjar

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ export PATH=${JAVA_HOME}/bin:${PATH}
 
 export TASKSD_PIDFILE=$(pwd)/taskd.pid
 export TASKS_CLIENT_SECRET="$(pwd)/client_secret.json"
-mvn clean install
+mvn clean -Dquarkus.package.type=uber-jar install
