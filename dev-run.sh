14f97782359383f1ec30fe5b4f3c06a4cb531772
Romain Pelisse <belaran@redhat.com>
2025-06-13 22:46:47 +0200

    Update dev-run.sh (out of sync)

--- dev-run.sh
+++ dev-run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-export JAVA_HOME=${JAVA_HOME:-'/usr/lib/jvm/java-11'}
-export MAVEN_HOME=${MAVEN_HOME:-'/home/rpelisse/Products/tools/apache-maven-3.6.3'}
-export PATH=${JAVA_HOME}/bin:${MAVEN_HOME}/bin:${PATH}
-
+export JAVA_HOME="${JAVA_HOME:-/usr/lib/jvm/jre-17}"
+export MAVEN_HOME="${MAVEN_HOME:-/opt/java/apache-maven-3.9.10}"
+export PATH="${JAVA_HOME}/bin:${MAVEN_HOME}/bin:${PATH}"
 
 export TASKSD_PIDFILE=$(pwd)/taskd.pid
-export TASKS_CLIENT_SECRET="$(pwd)/client_secret.json"
-mvn clean compile quarkus:dev -Dquarkus.http.port=8083
+export TASKS_CLIENT_SECRET='/opt/gtasks-proxy/client_secret.json'
+${MAVEN_HOME}/bin/mvn clean compile quarkus:dev -Dquarkus.http.port=8083
