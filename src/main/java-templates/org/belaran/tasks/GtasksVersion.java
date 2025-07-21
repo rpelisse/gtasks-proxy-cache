59e48ba78ab379f4f070cfa90c4cc538e1be346e
Romain Pelisse <belaran@redhat.com>
2025-07-21 18:35:19 +0200

    Add Quarkus version to version

--- src/main/java-templates/org/belaran/tasks/GtasksVersion.java
+++ src/main/java-templates/org/belaran/tasks/GtasksVersion.java
@@ -2,4 +2,6 @@ package org.belaran.tasks;
 
 public class GtasksVersion {
     public static final String VERSION = "${gtasks.version}";
+
+    public static final String QUARKUS_VERSION = "${quarkus.version}";
 }
