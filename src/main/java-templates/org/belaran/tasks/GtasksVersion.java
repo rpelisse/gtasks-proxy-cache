4da7d5ab06e58ee19b896a5ba9595580e0af1d2f
Romain Pelisse <belaran@gmail.com>
2025-09-24 12:07:48 +0200

    add GTasks API version to -V output

--- src/main/java-templates/org/belaran/tasks/GtasksVersion.java
+++ src/main/java-templates/org/belaran/tasks/GtasksVersion.java
@@ -4,4 +4,6 @@ public class GtasksVersion {
     public static final String VERSION = "${gtasks.version}";
 
     public static final String QUARKUS_VERSION = "${quarkus.version}";
+
+    public static final String GTASKS_VERSION = "${google.tasks.version}";
 }
