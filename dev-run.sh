7ee82c0f3914dd574c5c013a02437b850d7daa51
Romain Pelisse <belaran@gmail.com>
2025-12-25 11:43:58 +0100

    dev-run.sh: switch defaults for java and mvn

--- dev-run.sh
+++ dev-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export JAVA_HOME="${JAVA_HOME:-/usr/lib/jvm/jre-17}"
-export MAVEN_HOME="${MAVEN_HOME:-/opt/java/apache-maven-3.9.10}"
+export JAVA_HOME="${JAVA_HOME:-/usr/lib/jvm/java}"
+export MAVEN_HOME="${MAVEN_HOME:-/usr/share/maven}"
 export PATH="${JAVA_HOME}/bin:${MAVEN_HOME}/bin:${PATH}"
 
 export TASKSD_PIDFILE=$(pwd)/taskd.pid
