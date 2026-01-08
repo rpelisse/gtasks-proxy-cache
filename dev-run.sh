591ada2f308533bb4447f50a64c740d4065f6f84
Romain Pelisse <belaran@redhat.com>
2026-01-08 11:46:42 +0100

    fix port number

--- dev-run.sh
+++ dev-run.sh
@@ -5,4 +5,4 @@ export PATH="${JAVA_HOME}/bin:${MAVEN_HOME}/bin:${PATH}"
 
 export TASKSD_PIDFILE=$(pwd)/taskd.pid
 export TASKS_CLIENT_SECRET='/opt/gtasks-proxy/client_secret.json'
-${MAVEN_HOME}/bin/mvn clean compile quarkus:dev -Dquarkus.http.port=8083
+${MAVEN_HOME}/bin/mvn clean compile quarkus:dev -Dquarkus.http.port=2333
