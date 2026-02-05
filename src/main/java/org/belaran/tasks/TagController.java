5de03334767f0bf21ac5aef40c1ccc7dd242bce7
Romain Pelisse <romain.pelisse@buchner.de>
2026-02-05 08:49:55 +0100

    whitespaces

--- src/main/java/org/belaran/tasks/TagController.java
+++ src/main/java/org/belaran/tasks/TagController.java
@@ -8,38 +8,38 @@ import jakarta.inject.Singleton;
 @Singleton
 public class TagController {
 
-	private Map<String,String> tagsIndexedByName = new HashMap<String, String>(3);
+    private Map<String,String> tagsIndexedByName = new HashMap<String, String>(3);
 
-	public TagController() {
-		tagsIndexedByName.put("phone", "📞");
-		tagsIndexedByName.put("dollar", "💲");
-		tagsIndexedByName.put("blocker", "⛔");
-		tagsIndexedByName.put("rpg","🎲");
-		tagsIndexedByName.put("email", "✉️");
-		tagsIndexedByName.put("cat", "🐈");
-		tagsIndexedByName.put("music", "🎶");
-		tagsIndexedByName.put("house", "🏠");
-		tagsIndexedByName.put("print", "📄");
-		tagsIndexedByName.put("food", "🍆");
+    public TagController() {
+        tagsIndexedByName.put("phone", "📞");
+        tagsIndexedByName.put("dollar", "💲");
+        tagsIndexedByName.put("blocker", "⛔");
+        tagsIndexedByName.put("rpg","🎲");
+        tagsIndexedByName.put("email", "✉️");
+        tagsIndexedByName.put("cat", "🐈");
+        tagsIndexedByName.put("music", "🎶");
+        tagsIndexedByName.put("house", "🏠");
+        tagsIndexedByName.put("print", "📄");
+        tagsIndexedByName.put("food", "🍆");
         tagsIndexedByName.put("medics","💊");
         tagsIndexedByName.put("write","✏️");
         tagsIndexedByName.put("drums","🥁");
-	}
+    }
 
-	public Map<String, String> getTagsIndexedByName() {
-		return tagsIndexedByName;
-	}
+    public Map<String, String> getTagsIndexedByName() {
+        return tagsIndexedByName;
+    }
 
-	public static String tagTaskTitle(String symbol, String title) {
-		return symbol + " " + title;
-	}
+    public static String tagTaskTitle(String symbol, String title) {
+        return symbol + " " + title;
+    }
 
-	public String getSymbolForTag(String tag) {
-		return tagsIndexedByName.get(getKeyAssociatedToTagId(tag));
-	}
+    public String getSymbolForTag(String tag) {
+        return tagsIndexedByName.get(getKeyAssociatedToTagId(tag));
+    }
 
-	public String getKeyAssociatedToTagId(String tag) {
-		return tagsIndexedByName.keySet().stream().filter(key -> key.startsWith(tag)).findFirst()
-				.orElseThrow(() -> new IllegalArgumentException("No tag associated to :" + tag));
-	}
+    public String getKeyAssociatedToTagId(String tag) {
+        return tagsIndexedByName.keySet().stream().filter(key -> key.startsWith(tag)).findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No tag associated to :" + tag));
+    }
 }
