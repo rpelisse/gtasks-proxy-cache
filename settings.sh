ccafd438f3400eaa9995b2521ab2400b720fbea9
Romain Pelisse <romain.pelisse@buchner.de>
2026-02-05 08:49:06 +0100

    add a local settings file to allow jdk/mvn home override easily

--- settings.sh
+++ settings.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+readonly LOCAL_ENV_FILE=${LOCAL_ENV_FILE:-'settings_local.sh'}
 export JAVA_HOME=${JAVA_HOME:-'/usr/lib/jvm/default-runtime/'}
 export MAVEN_HOME=${MAVEN_HOME:-'/usr/share/java/maven/'}
+if [ -e "${LOCAL_ENV_FILE}" ]; then
+  source "${LOCAL_ENV_FILE}"
+fi
 export PATH=${JAVA_HOME}/bin:${MAVEN_HOME}/bin:${PATH}
