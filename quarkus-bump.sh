e25b991f12459b9b0e5a5128651ad5709130d115
Romain Pelisse <belaran@gmail.com>
2026-06-25 11:38:35 +0200

    quarkus bump: align gtasks to quarkus version by default

--- quarkus-bump.sh
+++ quarkus-bump.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 readonly QUARKUS_VERSION=${1}
-readonly GTASKS_VERSION=${2}
-
-if [ -z "${GTASKS_VERSION}" ]; then
-  echo "No GTASKS_VERSION defined, aborting."
-  exit 1
-fi
 
 if [ -z "${QUARKUS_VERSION}" ]; then
   echo "No QUARKUS_VERSION defined, aborting."
-  exit 2
+  exit 1
+fi
+
+if [ -z "${2}" ]; then
+  echo "No GTASKS_VERSION defined, reusing Quarkus version ${QUARKUS_VERSION}."
+  GTASKS_VERSION=${QUARKUS_VERSION}
+else
+    readonly GTASKS_VERSION=${2}
 fi
 
 sed -i pom.xml -e "s/\(<gtasks.version>\)[0-9\.]*/\1${GTASKS_VERSION}/" \
